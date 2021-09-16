40f85ab19072c3ef55f6651203c757e54b6d163b
OMAR-FARUK-SHAKIL <omarshakil8@gmail.com>
2021-09-16 07:03:47 +0600

    back end side developed

---
+++ README.md
@@ -0,0 +1,88 @@
+## memeVerse- Animage sharing app
+
+### Key Feature -
+
+    * Blogger Registration
+    * Commentor Registration
+    * API token
+
+### Role -
+
+    * Bloger -
+    [ ] Can post new blog
+    [ ] Update, delete old blog
+    [ ] Read other bloggers blog
+    [ ] Can't update delete others blog
+    [ ] Can mark unmark a commentator as spammer
+    [ ] Can delete all comments of a blog post
+    [ ] Can stop comment of a post
+    [ ] Can schedule turn off commenting
+    [ ] Can mark their post commentor as Feature/top commentor
+    [ ] Can up vote / down vote of a post and comment
+
+    * Commentator-
+    [ ] Can comment on post
+    [ ] Can create update and delete their comment
+    [ ] Can read blog post and other commentor comment
+    [ ] Can't delete update other commentor comment
+    [ ] Can up vote / down vote post and comment
+
+    * Additional Feature -
+    [ ] Comment reply nested to 3 reply
+    [ ] Blog post and comment should sorted by time
+    [ ] Blog post and comment should have pagination
+
+### Work Strategy -
+
+    * Build the frontend
+        * Components Structure -
+            * Login Page
+            * public profile page
+            * Blog Page
+                * New Blog
+                * Blog Component
+                * Title, Author, Time
+                * Feature Image
+                * Content
+                * Upvote/downvote
+                * Comment
+                    * New Comment
+                    * Old Comment
+
+### Plan
+
+    [x] Register and login page
+    [x] Private Secured route
+    [x] New Blog
+    [x] Show blog
+    [ ] New Comment
+    [ ] Show comment
+    [ ] User permission
+    [ ] Update blog
+    [ ] Delete Blog
+    [ ] Update comment
+    [ ] delete comment
+    [ ] upvote downvote
+    ## Blog Application
+
+### Work Plan -
+
+    [x] Create New Blog
+    [x] Show Short Blog
+    [x] Show Full Blog
+    [x] Show Nesting Comment
+    [x] Create New Comment
+    [x] Connect comment to parent
+    [x] Show Up Vote
+    [x] Show Down Vote
+    [x] User Registration
+    [x] Edit blog done
+    [x] Delete Blog
+    [x] Turn On/Off reply of a specific post
+    [x] Delete all comment of a post
+    [x] Edit Comment
+    [x] Delete Comment
+    [ ] mark as spamm
+    [ ] schedule comment turn off
+    [ ] Feature comment
+    [ ] banned spammer from app
---
+++ index.js
@@ -0,0 +1,332 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const fs = require('fs')
+const fileupload = require('express-fileupload')
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID
+
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('serviceimg'))
+app.use(fileupload())
+const port = 2555;
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wj6rs.mongodb.net/vaccineAtHome?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xzynl.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+    console.log(err);
+    const userCollection = client.db(`${process.env.DATABASE}`).collection("userCollection");
+    const blogCollection = client.db(`${process.env.DATABASE}`).collection("blogCollection");
+    const commentCollection = client.db(`${process.env.DATABASE}`).collection("commentCollection");
+    const questionCollection = client.db(`${process.env.DATABASE}`).collection("questionCollection");
+    const answerCollection = client.db(`${process.env.DATABASE}`).collection("answerCollection");
+
+
+    app.get('/', (req, res) => {
+        res.send('Hello World!')
+    })
+
+
+    app.post('/login', (req, res) => {
+        console.log('login req recieved')
+        userCollection.findOne({ email: req.body.email }, (err, doc) => {
+            if (doc) {
+                console.log('email address found')
+                console.log(req.body.password)
+                console.log(doc.password)
+                if (doc.password === req.body.password) {
+                    console.log('password matched')
+                    const userInfo = {
+                        fullName: doc.fullName,
+                        userName: doc._id,
+                        role: doc.role,
+                        photo: doc.photo,
+                        spamcount: doc.spamcount
+                    }
+                    res.send(userInfo)
+                } else {
+                    res.send(false)
+                }
+            } else {
+                res.send(false)
+            }
+        })
+    })
+
+    // Register User
+    app.post('/register', (req, res) => {
+        console.log(req.body.email)
+        const user = req.body
+        userCollection.findOne({ email: req.body.email }, (err, doc) => {
+            if (!doc) {
+                console.log('unique email')
+                userCollection.insertOne(user)
+                    .then(result => {
+                        console.log(result.insertedCount)
+                        res.send(result.insertedCount > 0)
+                    })
+            } else {
+                res.send(false)
+            }
+        })
+    })
+
+    // Check Existing User
+    app.post('/users', (req, res) => {
+        const email = req.body.email;
+        userCollection.findOne({ email: email }, (err, doc) => {
+            if (doc) {
+                res.send(doc)
+            } else {
+                res.send(false)
+            }
+        })
+    })
+
+    // Create New User
+    app.post('/createUser', (req, res) => {
+        userCollection.insertOne(req.body)
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
+
+    // Get Public Profile Info
+    app.get('/profile/:userName', (req, res) => {
+        const userName = req.params.userName;
+        userCollection.findOne({ _id: userName }, (err, doc) => {
+            if (doc) {
+                const publicProfile = {
+                    fullName: doc.fullName,
+                    photo: doc.photo,
+                    role: doc.role
+                }
+                res.send(publicProfile)
+            } else {
+                res.send(false)
+            }
+        })
+    })
+
+    // Add Blog Post
+    app.post('/addBlog', (req, res) => {
+        const blog = req.body
+        blogCollection.insertOne(blog)
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
+
+    // Get Blog Post
+    app.get('/getBlogs', (req, res) => {
+        blogCollection.find()
+            .toArray((err, docs) => res.send(docs))
+    })
+
+    // Get Blog Post
+    app.get('/blog/:id', (req, res) => {
+        blogCollection.findOne({ _id: ObjectID(req.params.id) }, (err, doc) => {
+            if (doc) {
+                res.send(doc)
+            } else {
+                res.send(false)
+            }
+        })
+    })
+    // Get Comment
+    app.get('/getComment/:id', (req, res) => {
+        commentCollection.findOne(({ _id: req.params.id }), (err, doc) => {
+            if (doc) {
+                res.send(doc)
+            } else {
+                res.send(false)
+            }
+        })
+    })
+
+    // Add Comment Reply reference
+    app.patch('/updateParent/:id', (req, res) => {
+        const reply = req.body.reply
+        commentCollection.updateOne({ _id: req.params.id },
+            {
+                $set: { 'reply': reply }
+            }
+        ).then(result => {
+            res.send(result.modifiedCount > 0)
+        })
+    })
+    // Add Comment Reply reference to blog
+    app.patch('/updateBlogParent/:id', (req, res) => {
+        const reply = req.body.reply
+        blogCollection.updateOne({ _id: ObjectID(req.params.id) },
+            {
+                $set: { 'reply': reply }
+            }
+        ).then(result => {
+            res.send(result.modifiedCount > 0)
+        })
+    })
+
+    // Add Comment Reply
+    app.post('/addComment', (req, res) => {
+        const comment = req.body
+        commentCollection.insertOne(comment)
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+        res.send(true)
+    })
+
+    //  Blog Up Vote
+    app.patch('/updateVote/:id', (req, res) => {
+        blogCollection.updateOne({ _id: ObjectID(req.params.id) }, {
+            $set: req.body
+        }).then(result => {
+            res.send(result.modifiedCount > 0)
+        })
+    })
+
+
+
+    // Update
+    app.patch('/updateBlog/:id', (req, res) => {
+        const title = req.body.title;
+        const tag = req.body.tag;
+        const content = req.body.content;
+        blogCollection.updateOne({ _id: ObjectID(req.params.id) }, {
+            $set: { title: title, tag: tag, content: content }
+        }).then(result => {
+            res.send(result.modifiedCount > 0)
+        })
+    })
+    // Update
+
+    // Delete Blog
+    app.delete('/deleteBlog/:id', (req, res) => {
+        blogCollection.deleteOne({ _id: ObjectID(req.params.id) })
+            .then(result => {
+                res.send(result.deletedCount > 0);
+            })
+    })
+    // Delete Blog
+
+    // Update Blog Reply Status
+    app.patch('/updateReplyStatus/:id', (req, res) => {
+        blogCollection.updateOne({ _id: ObjectID(req.params.id) }, {
+            $set: { replyStatus: req.body.replyStatus }
+        }).then(result => {
+            res.send(result.modifiedCount > 0)
+        })
+    })
+    // Update Blog Reply Status
+
+    // Delete Comment
+    app.delete('/deleteComment/:id', (req, res) => {
+        commentCollection.deleteOne({ _id: req.params.id })
+            .then(result => {
+                res.send(result.deletedCount > 0)
+            })
+    })
+
+    // Update Comment
+    app.patch('/updateComment/:id', (req, res) => {
+        commentCollection.updateOne({ _id: req.params.id }, {
+            $set: { comment: req.body.comment }
+        })
+            .then(result => {
+                res.send(result.modifiedCount > 0)
+                console.log(result.modifiedCount)
+            })
+    })
+
+    // toggle Feature Comment 
+    app.patch('/toggleFeatureComment/:id', (req, res) => {
+        commentCollection.updateOne({ _id: req.params.id }, {
+            $set: req.body
+        })
+            .then(result => {
+                res.send(result.modifiedCount > 0)
+                console.log(result.modifiedCount)
+            })
+    })
+
+    // Update Spam Count
+    app.patch(`/spamCount/:id`, (req, res) => {
+        userCollection.findOne({ _id: req.params.id }, (err, doc) => {
+            if (doc) {
+                if (req.body.count) {
+                    const newCount = { spamcount: doc.spamcount + 1 }
+                    userCollection.updateOne({ _id: req.params.id }, {
+                        $set: newCount
+                    }).then(result => {
+                        res.send(result.modifiedCount > 0)
+                    })
+                }
+                if (!req.body.count) {
+                    const newCount = { spamcount: doc.spamcount - 1 }
+                    userCollection.updateOne({ _id: req.params.id }, {
+                        $set: newCount
+                    }).then(result => {
+                        res.send(result.modifiedCount > 0)
+                    })
+                }
+            } else {
+                console.log(err)
+            }
+        })
+    })
+
+
+    // AskAnything App Method
+    app.get('/allQuestions', (req, res) => {
+        questionCollection.find()
+            .toArray((err, docs) => res.send(docs))
+    })
+
+    app.get('/getAnswer/:id', (req, res) => {
+        answerCollection.find({ parent: req.params.id })
+            .toArray((err, docs) => res.send(docs))
+    })
+    app.post('/addAnswer', (req, res) => {
+        answerCollection.insertOne(req.body)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+    app.post('/addQuestion', (req, res) => {
+        questionCollection.insertOne(req.body)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    app.patch('/updateQuetion/:id', (req, res) => {
+        questionCollection.updateOne({ _id: req.params.id }, {
+            $set: req.body
+        }).then(result => {
+            res.send(result.modifiedCount > 0)
+        })
+    })
+    app.patch('/updateAnswer/:id', (req, res) => {
+        answerCollection.updateOne({ _id: req.params.id }, {
+            $set: req.body
+        }).then(result => {
+            res.send(result.modifiedCount > 0)
+        })
+    })
+
+})
+
+app.listen(process.env.PORT || port, () => {
+    console.log('server running ..')
+})
